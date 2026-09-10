1d39471194004b3fb78e0ffee12c839b9376e288
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2026-09-10 10:07:55 +0900

    [修正]

--- js/common/common.js
+++ js/common/common.js
@@ -1,7 +1,7 @@
 document.addEventListener(
     'DOMContentLoaded', () =>
     {
-        const basePath = '/components/';
+        const basePath = '/html/components/';
 
         // ヘッダーの読み込み
         fetch(basePath + 'header.html')
